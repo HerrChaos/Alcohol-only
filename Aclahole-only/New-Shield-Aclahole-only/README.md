7214a6fcf21664864c5517bfce6725830d57d759
Herr Chaos <138036657+HerrChaos@users.noreply.github.com>
2023-11-18 01:26:17 +0100

    Delete Aclahole-only/New-Shield-Aclahole-only directory

--- Aclahole-only/New-Shield-Aclahole-only/README.md
+++
@@ -1,5 +0,0 @@
-New Shields and problebly some other shit to
-
-This mod is made with a small intent of joining rats mc server.
-
-so could i pwease join? 👉👈
--- Aclahole-only/New-Shield-Aclahole-only/src/client/java/charten/shield/ExampleModClient.java
+++
@@ -1,9 +0,0 @@
-package charten.shield;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class ExampleModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
