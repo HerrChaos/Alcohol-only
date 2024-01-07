20ea17472768bbd1075594d3b2ce17cd7692d61d
HerrChaos <cedrik.zwick@gmx.net>
2024-01-07 23:20:02 +0100

    fixed the messy and buggy full beer block r click code

--- src/main/java/charten/shield/block/custom/FullBeerGlassBlock.java
+++ src/main/java/charten/shield/block/custom/FullBeerGlassBlock.java
@@ -1,10 +1,11 @@
 package charten.shield.block.custom;
 
+import charten.shield.statuseffect.ModStatusEffects;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.ActionResult;
@@ -15,6 +16,8 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
+import java.util.Objects;
+
 import static charten.shield.block.ModBlocks.BEER_GLASS_BLOCK;
 import static net.minecraft.sound.SoundEvents.ENTITY_GENERIC_DRINK;
 
@@ -24,32 +27,33 @@ public class FullBeerGlassBlock extends Block{
     public FullBeerGlassBlock(Settings settings) {
         super(settings);
     }
-
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return ONE_BOTTLE_SHAPE;
     }
 
+
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (player.getStackInHand(hand).isEmpty()) {
-            player.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 2000, 1));
-            player.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2000, 1));
-            player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1));
-            player.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 2000, 1));
-            player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1000, 1));
+            StatusEffectInstance apliedStatuseffect = player.getStatusEffect(ModStatusEffects.Drunkinnis);
+            if (apliedStatuseffect != null) {
+                if (apliedStatuseffect.getAmplifier() >= 5) {
+                    player.addStatusEffect(new StatusEffectInstance(ModStatusEffects.Drunkinnis, 20 * 20, 5));
+                }
+                player.addStatusEffect(new StatusEffectInstance(ModStatusEffects.Drunkinnis, 20 * 20, apliedStatuseffect.getAmplifier() + 1));
+            } else {
+                player.addStatusEffect(new StatusEffectInstance(ModStatusEffects.Drunkinnis, 20 * 20, 0));
+            }
+
+            for (Pair<StatusEffectInstance, Float> effect : Objects.requireNonNull(this.asItem().getFoodComponent()).getStatusEffects()) {
+                player.addStatusEffect(effect.getFirst());
+            }
+
             world.playSound(player, pos, ENTITY_GENERIC_DRINK, SoundCategory.PLAYERS,1,1);
             world.setBlockState(pos, BEER_GLASS_BLOCK.getDefaultState());
             return ActionResult.success(world.isClient);
         }
         return ActionResult.PASS;
     }
-
-/*
-    statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 2000), 1f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2000), 1f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200), 1f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1000, 1), 1f))
-
- */
 }
